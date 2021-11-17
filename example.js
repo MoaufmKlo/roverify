0552d32280fe4568f95f384c67a1146f5082ce72
MoaufmKlo <45636897+MoaufmKlo@users.noreply.github.com>
2021-11-17 19:49:55 +0100

    Refactor code to fit to project code style

--- example.js
+++ example.js
@@ -1,34 +1,32 @@
-// Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
 const roverify = require("roverify");
 
-// Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGES.GUILD _MESSAGES] });
+const discord = require("discord.js").Client;
+const client = new Client({
+    "intents": [
+        "GUILDS",
+        "GUILDS_MESSAGES"
+    ]
+});
 
-// When the client is ready, run this code (only once)
 client.once("ready", () => {
-	console.log("Ready");
+	console.log(`Connection to Discord established as ${client.user.tag}.`);
 });
 
 client.on("messageCreate", msg => {
-  if(msg.content == "verify") {
-    var userId = msg.user.id
-    roverify.verify(userId)    
-      .then((robloxUser) => {
-          msg.setNickname(robloxUser.name)
-          msg.editReply(`Successfully Verified, **${robloxUser.name}!**`)
-          // Assuming you're bot has permissions.
-      })
-      .catch((err) => {
-          if (err === "Discord user id (argument 0) is not verified") {
-              msg.editReply(`Hey! You're not verified. Try verifying through rover here!\n\nhttp://rover.link/verify`)
-          } else {
-              throw err;
-          }
-      });
-  }
-})
+    if (msg.content !== "!verify") return;
+    
+    roverify.verify(msg.author.id)    
+        .then((robloxUser) => {
+            msg.setNickname(robloxUser.name);
+            msg.reply(`Verified as ${robloxUser.name}`);
+        })
+        .catch((err) => {
+            if (err === "Discord user id (argument 0) is not verified") {
+                msg.reply("You're not verified yet. Go to <https://blox.link/verify> to verify.")
+            } else {
+                throw err;
+            }
+        });
+});
 
-// Login to Discord with your client's token
-client.login(token);
+client.login();
