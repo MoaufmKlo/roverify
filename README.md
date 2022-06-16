29e8f15796c6cbf31956ffc542da4c7542d3f85c
MoaufmKlo <45636897+MoaufmKlo@users.noreply.github.com>
2022-06-16 07:38:05 +0200

    fix: remove Bloxlink

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-<div align="center">
-    <h1>roverify</h1>
-    <img alt="npm Version" src="https://img.shields.io/npm/v/roverify?style=for-the-badge">
-    <img alt="npm Downloads" src="https://img.shields.io/npm/dw/roverify?style=for-the-badge">
-    <img alt="Checks" src="https://img.shields.io/github/checks-status/moaufmklo/roverify/main?style=for-the-badge">
-    <br>
+<div align='center'>
+  <h1>roverify</h1>
+  <img alt='npm Version' src='https://img.shields.io/npm/v/roverify?style=for-the-badge'>
+  <img alt='npm Downloads' src='https://img.shields.io/npm/dw/roverify?style=for-the-badge'>
+  <img alt='Checks' src='https://img.shields.io/github/checks-status/moaufmklo/roverify/main?style=for-the-badge'>
+  <br>
 </div>
 
 ## Table of contents
@@ -16,7 +16,7 @@
 
 ## About
 
-4.43kB & dependency-free [Node.js](https://nodejs.org/) module to fetch Roblox user data from a Discord user. The module prioritizes Bloxlink over RoVer.
+4.43kB & dependency-free [Node.js](https://nodejs.org/) module to fetch Roblox user data from a Discord user. The module prioritizes RoVer over all other services.
 
 ## Installation
 
@@ -29,33 +29,30 @@ npm install roverify
 ## Usage
 
 ```javascript
-const roverify = require("roverify");
+const roverify = require('roverify');
 
-roverify.verify("113691352327389188")    
-    .then((robloxUser) => {
-        console.log(robloxUser.name);
-    })
-    .catch((err) => {
-        if (err === "Discord user id (argument 0) is not verified") {
-            console.log("Discord user is not verified");
-        } else {
-            throw err;
-        }
-    });
+await roverify.verify('113691352327389188')    
+  .then((robloxUser) => {
+      console.log(robloxUser.name)
+  })
+  .catch((err) => {
+      if (err === 'Discord user id (argument 0) is not verified') console.log('Discord user is not verified')
+      else throw err
+  })
 ```
 
 The `robloxUser` object returned consists of the following:
 
 ```javascript
 {
-    "description": "Welcome to the Roblox profile! This is where you can check out the newest items in the catalog, and get a jumpstart on exploring and building on our Imagination Platform. If you want news on updates to the Roblox platform, or great new experiences to play with friends, check out blog.roblox.com. Please note, this is an automated account. If you need to reach Roblox for any customer service needs find help at www.roblox.com/help",
-    "created": "2006-02-27T21:06:40.3Z",
-    "isBanned": false,
-    "externalAppDisplayName": null,
-    "id": 1,
-    "name": "Roblox",
-    "displayName": "Roblox",
-    "verificationService": "bloxlink"
+  'description': 'Welcome to the Roblox profile! This is where you can check out the newest items in the catalog, and get a jumpstart on exploring and building on our Imagination Platform. If you want news on updates to the Roblox platform, or great new experiences to play with friends, check out blog.roblox.com. Please note, this is an automated account. If you need to reach Roblox for any customer service needs find help at www.roblox.com/help',
+  'created': '2006-02-27T21:06:40.3Z',
+  'isBanned': false,
+  'externalAppDisplayName': null,
+  'id': 1,
+  'name': 'Roblox',
+  'displayName': 'Roblox',
+  'verificationService': 'bloxlink'
 }
 ```
 
--- index.js
+++ index.js
@@ -18,15 +18,15 @@ const https = require("https");
  * @returns {Promise<Response, Object>} Request response object
  */
 function get(url) {
-    return new Promise((res, rej) => https.get(url, (req) => {
-        let data = "";
-        req.on("data", (chunk) => data += chunk);
+  return new Promise((res, rej) => https.get(url, (req) => {
+    let data = "";
+    req.on("data", (chunk) => data += chunk);
 
-        req.on("end", () => {
-            req.json = JSON.parse(data);
-            res(req);
-        });
-    }));
+    req.on("end", () => {
+      req.json = JSON.parse(data);
+      res(req);
+    });
+  }));
 }
 
 /**
@@ -35,87 +35,63 @@ function get(url) {
  * @returns {Promise<RobloxUser>} Roblox user object
  */
 function fetchRoblox(robloxId) {
-    return new Promise((res, rej) => {
-        if (!robloxId) rej("Roblox user id (argument 0) missing or null");
+  return new Promise((res, rej) => {
+    if (!robloxId) rej("Roblox user id (argument 0) missing or null");
 
-        get(`https://users.roblox.com/v1/users/${robloxId}`).then((req) => {
-            if (req.statusCode === 200) {
-                req.json.created = new Date(req.json.created);
-                res(req.json);
-            } else {
-                rej(`${req.statusMessage} (${req.statusCode})`);
-            }
-        }).catch((err) => {
-            if (req.statusCode === 404) {
-                rej("Roblox user id (argument 0) is invalid");
-            } else {
-                rej(`${err.statusMessage} (${err.statusCode})`);
-            }
-        });
+    get(`https://users.roblox.com/v1/users/${robloxId}`).then((req) => {
+      if (req.statusCode === 200) {
+        req.json.created = new Date(req.json.created);
+        res(req.json);
+      } else {
+        rej(`${req.statusMessage} (${req.statusCode})`);
+      }
+    }).catch((err) => {
+      if (req.statusCode === 404) {
+        rej("Roblox user id (argument 0) is invalid");
+      } else {
+        rej(`${err.statusMessage} (${err.statusCode})`);
+      }
     });
+  });
 }
 
 module.exports = {
 
-    /**
-     * Get a Discord user's Roblox account.
-     * @param {String} discordId Discord user id
-     * @returns {Promise<RobloxUser>} Roblox user object
-     */
-    "verify": (discordId) => {
-        return new Promise((res, rej) => {
-            if (!discordId) rej("Discord user id (argument 0) missing or null");
+  /**
+   * Get a Discord user's Roblox account.
+   * @param {String} discordId Discord user id
+   * @returns {Promise<RobloxUser>} Roblox user object
+   */
+  "verify": (discordId) => {
+    return new Promise((res, rej) => {
+      if (!discordId) rej("Discord user id (argument 0) missing or null");
 
-            // Attempt 1: Bloxlink
-            get(`https://api.blox.link/v1/user/${discordId}`).then((req) => {
-                const bloxlink = req.json;
+      // Attempt 1: RoVer
+      get(`https://verify.eryn.io/api/user/${discordId}`).then((req) => {
+        const rover = req.json;
 
-                if (bloxlink.status === "ok") {
-                    fetchRoblox(bloxlink.primaryAccount)
-                        .then((user) => {
-                            user.verificationService = "bloxlink";
-                            res(user);
-                        })
-                        .catch((err) => {
-                            rej(err);
-                        });
-                } else if (bloxlink.status === "error") {
-                    if (bloxlink.error === "This user is not linked with Bloxlink.") {
-                        // Attempt 2: RoVer
-                        get(`https://verify.eryn.io/api/user/${discordId}`).then((req) => {
-                            const rover = req.json;
-
-                            if (rover.status === "ok") {
-                                fetchRoblox(rover.robloxId)
-                                    .then((user) => {
-                                        user.verificationService = "rover";
-                                        res(user);
-                                    })
-                                    .catch((err) => {
-                                        rej(`${err.statusMessage} (${err.statusCode})`);
-                                    });
-                            } else if (rover.status === "error") {
-                                if (rover.error === "User not found.") {
-                                    rej("Discord user id (argument 0) is not verified");
-                                } else {
-                                    rej(rover.error);
-                                }
-                            } else {
-                                rej(rover.status);
-                            }
-                        }).catch((err) => {
-                            rej(`${err.statusMessage} (${err.statusCode})`);
-                        });;
-                    } else {
-                        rej(bloxlink.error);
-                    }
-                } else {
-                    rej(bloxlink.status);
-                }
-            }).catch((err) => {
-                rej(`${err.statusMessage} (${err.statusCode})`);
+        if (rover.status === "ok") {
+          fetchRoblox(rover.robloxId)
+            .then((user) => {
+              user.verificationService = "rover";
+              res(user);
+            })
+            .catch((err) => {
+              rej(`${err.statusMessage} (${err.statusCode})`);
             });
-        });
-    }
+        } else if (rover.status === "error") {
+          if (rover.error === "User not found.") {
+            rej("Discord user id (argument 0) is not verified");
+          } else {
+            rej(rover.error);
+          }
+        } else {
+          rej(rover.status);
+        }
+      }).catch((err) => {
+        rej(`${err.statusMessage} (${err.statusCode})`);
+      });;
+    });
+  }
 
 };
